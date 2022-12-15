1c5f3a2f9ab23b7b63c887ab917dcbc4c8083766
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2022-12-15 15:52:44 -0600

    remove comments

--- routes/main.js
+++ routes/main.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const Anime = require('../models/anime');
+const express = require("express");
+const Anime = require("../models/anime");
 const router = express.Router();
-const isLoggedIn = require('../utils/isLoggedIn');
+const isLoggedIn = require("../utils/isLoggedIn");
 
 router.get("/", async (req, res) => {
-    const anime = await Anime.find().exec()
-    res.render("landing", {anime});
-})
-
-router.get('/account', isLoggedIn, (req, res) => {
-    res.render("account")
+  const anime = await Anime.find().exec();
+  res.render("landing", { anime });
 });
 
-module.exports = router
+let url = "https://api.myanimelist.net/v2/anime?q=one&limit=4";
+
+router.get("/account", isLoggedIn, (req, res) => {
+  res.render("account");
+});
+
+module.exports = router;

26cd71a0fb6740fe261f0bbf85b482767d3efad5
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2023-01-09 16:56:52 -0600

    add and configured cors package

--- app.js
+++ app.js
@@ -3,36 +3,36 @@
 //=======================
 
 //NPM Imports
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override')
-const morgan = require('morgan')
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
-const expressSession = require('express-session')
-const flash = require('connect-flash')
-
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const expressSession = require("express-session");
+const flash = require("connect-flash");
+const cors = require("cors");
 
 //Config Imports
-try{
-    var config = require("./config");
+try {
+  var config = require("./config");
 } catch (err) {
-    console.log("Could not import config. Not working locally");
-    console.log(err)
+  console.log("Could not import config. Not working locally");
+  console.log(err);
 }
 
 //Model Imports
-const Anime = require('./models/anime');
-const Comment = require('./models/comment');
-const User = require('./models/user')
+const Anime = require("./models/anime");
+const Comment = require("./models/comment");
+const User = require("./models/user");
 
 //Route Imports
-const animeRoutes = require('./routes/animes')
-const commentRoutes = require('./routes/comments')
-const mainRoutes = require('./routes/main')
-const authRoutes = require('./routes/auth')
+const animeRoutes = require("./routes/animes");
+const commentRoutes = require("./routes/comments");
+const mainRoutes = require("./routes/main");
+const authRoutes = require("./routes/auth");
 
 //========================
 // DEVELOPMENT
@@ -50,32 +50,42 @@ const authRoutes = require('./routes/auth')
 
 //Connect to DB
 try {
-    mongoose.connect(config.db.connection, {useNewUrlParser: true, useUnifiedTopology: true});
+  mongoose.connect(config.db.connection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 } catch (err) {
-    console.log("Could not connect. Not Working Locally")
-    mongoose.connect(process.env.DB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
+  console.log("Could not connect. Not Working Locally");
+  mongoose.connect(process.env.DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
 
 // Express Config
 app.set("view engine", "ejs");
-app.use(express.static('public'));
-app.use(express.json({
-    type: ["application/json", "text/plain"]
-}))
+app.use(express.static("public"));
+app.use(
+  express.json({
+    type: ["application/json", "text/plain"],
+  })
+);
 
 //Express Session Config
 
-app.use(expressSession({
+app.use(
+  expressSession({
     secret: process.env.ES_SECRET || config.expressSession.secret,
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 
 //Body Parser Config
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Method Override Config
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
 //Connect Flash
 app.use(flash());
@@ -89,14 +99,21 @@ passport.use(new LocalStrategy(User.authenticate()));
 
 // State Config
 app.use((req, res, next) => {
-    res.locals.user = req.user;
-    res.locals.errorMessage = req.flash("error");
-    res.locals.successMessage = req.flash("success");
-    next();
-})
+  res.locals.user = req.user;
+  res.locals.errorMessage = req.flash("error");
+  res.locals.successMessage = req.flash("success");
+  next();
+});
+
+//CORS Config
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 //Route Config
-app.use("/", authRoutes)
+app.use("/", authRoutes);
 app.use("/", mainRoutes);
 app.use("/anime", animeRoutes);
 app.use("/anime/:id/comments", commentRoutes);
@@ -105,7 +122,6 @@ app.use("/anime/:id/comments", commentRoutes);
 // LISTEN
 //========================
 
-
 app.listen(process.env.PORT || 3000, () => {
-    console.log("yelp anime is running")
+  console.log("yelp anime is running");
 });
